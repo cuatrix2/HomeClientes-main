12a71256aa1d136394779218324243c2372f21fb
cuatrix2 <arielllerena2001@outlook.com>
2023-08-24 21:12:19 -0500

    Edicion de pagina clientes

---
+++ views/html/head.php
@@ -0,0 +1,18 @@
+<meta charset="utf-8">
+    <title>Gymnast - Gym Website Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free Website Template" name="keywords">
+    <meta content="Free Website Template" name="description">
+
+    <!-- Favicon -->
+    <link href="../../public/img/favicon.ico" rel="icon">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+
+    <!-- Flaticon Font -->
+    <link href="../../public/lib/flaticon/font/flaticon.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="../../public/css/style.min.css" rel="stylesheet">
+    <!-- Incons-->
+
